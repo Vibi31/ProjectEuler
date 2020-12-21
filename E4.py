4b95f227fa62be296d17b2055558f495237b8eab
vibishar <vibishar@mail.uio.no>
2020-12-21 15:50:30 +0100

    finished 4E

--- E4.py
+++ E4.py
@@ -1,12 +1,13 @@
 
-m1 = 999
-m2 = 999
-print(m1*m2)
+
 def palindrom():
     M = 999**2 #larges possible value
     for i in range(M, 1, -1):
-        reverse =[]
-        normal = []
-        for num in i:
-            normal.append(num)
-         
+        i = str(i)
+        normal = [int(d) for d in i]
+        reverse = normal[::-1]
+        if reverse == normal:
+            return normal
+            break
+
+print(palindrom())
