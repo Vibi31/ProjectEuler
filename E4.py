75480c23b06f2d34ba93ebf76bf53ac277eea2d7
vibishar <vibishar@mail.uio.no>
2020-12-21 15:34:04 +0100

    not done

---
+++ E4.py
@@ -0,0 +1,12 @@
+
+m1 = 999
+m2 = 999
+print(m1*m2)
+def palindrom():
+    M = 999**2 #larges possible value
+    for i in range(M, 1, -1):
+        reverse =[]
+        normal = []
+        for num in i:
+            normal.append(num)
+         
