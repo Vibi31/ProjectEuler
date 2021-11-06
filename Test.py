3e4d8bf5dc0fb024a306c6c30879de9b470fa4b2
Vibi31 <vrvichu0@gmail.com>
2021-11-06 11:50:02 +0100

    ????

--- Test.py
+++ Test.py
@@ -1,30 +1,18 @@
-air_res = 0.83  #air resistce for cylindric objects
-air_den = 1.225 #air density
-surf_a  = W*D   #surface area affected by air resistance
+import matplotlib.pyplot as plt
+import numpy as np
 
-def tot_air_res(v):
-    return -1/2*(air_den*air_den*surf_a)*v**2
+#oppgave 2 plot
+plt.title('Aluminium filters of varying thickness. 60cm from radiation source for 30second ')
+plt.ylabel('charge [nC]')
+plt.xlabel('filter thickness [mm]')
+x = np.array([0.50, 1.00, 1.50, 2.00, 3.00, 4.03, 5.03])
+y = np.array([-7.28, -5.99, -5.09, -4.52, -3.62, -3.09, -2.66]) # Effectively y = x**2
+plt.plot(x, y)
+#e = np.array([0.07, 0.01, 0.01])
+#gradient 
+m, b = np.polyfit(x, y, 1)
+#plt.plot(x, m*x + b)
 
-def gravity(h):
-    return -G*m/(radius+h)**2
-
-def net_force(v, h):
-    return(tot_air_res(v) + gravity(h))
-
-time = 5000
-N = time/dt
-
-x = np.zeros(N)         #posisjon
-v = np.zeros(N)         #hastighet
-a = np.zeros(N)         #akselerasjon
-F = np.zeros(N)         #kraft
-
-t = np.linspace(0,10,N) #tids array der dt = 0.01sekunder
-for n in range (N-1):
-
-    F[n] = net_force(v[n], h[n])
-    a[n+1] = F/m
-    v[n+1] = v[n] + a[n]*(t[n+1]-t[n])
-    x[n+1] = x[n] + v[n]*(t[n+1]-t[n])
-
-https://socratic.org/questions/an-electromagnet-can-exert-a-lifting-force-of-12-0n-when-it-operates-from-a-curr
+#plt.errorbar(x, y, e, linestyle='None', marker='^')
+print('nC=',m,'time +',b)
+plt.show()
